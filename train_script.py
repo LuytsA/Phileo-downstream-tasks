15980f8804aa63b9a47304e06943fa3ab8a777b1
91097luke <lukecamilleri97@gmail.com>
2023-07-06 12:51:42 +0000

    + added data protocol script

--- train_script.py
+++ train_script.py
@@ -14,6 +14,7 @@ from utils import (
     load_data,
     training_loop,
     TiledMSE,
+    data_protocol_bd
 )
 
 
@@ -22,13 +23,13 @@ from utils import (
 if __name__ == "__main__":
 
     LEARNING_RATE = 0.0001
-    NUM_EPOCHS = 500
+    NUM_EPOCHS = 5
     BATCH_SIZE = 64
     REGIONS =['']#, 'east-africa', 'europe','eq-guinea', 'japan','south-america', 'nigeria', 'senegal']
     DATA_FOLDER = '/home/lcamilleri/data/s12_buildings/data_patches/'
-    model = ViT(chw=(10, 64, 64),  n_patches=4, n_blocks=2, hidden_d=768, n_heads=12)# SimpleUnet(input_dim=10, output_dim=1)
-    criterion = vit_mse_losses(n_patches=4)# nn.MSELoss()
-    lr_scheduler = 'reduce_on_plateau' # None, 'reduce_on_plateau', 'cosine_annealing'
+    model = SimpleUnet() #ViT(chw=(10, 64, 64),  n_patches=4, n_blocks=2, hidden_d=768, n_heads=12)# SimpleUnet(input_dim=10, output_dim=1)
+    criterion = nn.MSELoss() # vit_mse_losses(n_patches=4)
+    lr_scheduler = None # None, 'reduce_on_plateau', 'cosine_annealing'
     
     NAME = model.__class__.__name__
     OUTPUT_FOLDER = f'trained_models/{date.today().strftime("%d%m%Y")}_{NAME}'
@@ -39,16 +40,15 @@ if __name__ == "__main__":
 
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    dl_train, dl_val, dl_test = load_data(
-        x="s2",
-        y="y",
-        with_augmentations=False,
-        num_workers=0,
-        batch_size=BATCH_SIZE,
-        encoder_only=False,
-        folder=DATA_FOLDER,
-        regions=REGIONS
-    )
+
+    x_train, y_train, x_val, y_val, x_test, y_test = data_protocol_bd.protocol_split(folder=DATA_FOLDER,
+                                                                                     split_percentage=0.2)
+    dl_train, dl_val, dl_test = load_data(x_train, y_train, x_val, y_val, x_test, y_test,
+                                          with_augmentations=False,
+                                          num_workers=0,
+                                          batch_size=BATCH_SIZE,
+                                          encoder_only=False,
+                                          )
 
 
     wmape = torchmetrics.WeightedMeanAbsolutePercentageError(); wmape.__name__ = "wmape"
